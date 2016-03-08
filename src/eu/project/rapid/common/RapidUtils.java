0163c382972fa84ea743d380cac7ccaea40c731e
sokolkosta <kosta@di.uniroma1.it>
2016-03-08 14:50:25 +0800

    Added copyright header to java files.

--- src/eu/project/rapid/common/RapidUtils.java
+++ src/eu/project/rapid/common/RapidUtils.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (C) 2015, 2016 RAPID EU Project
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *******************************************************************************/
 package eu.project.rapid.common;
 
 import java.io.BufferedReader;
@@ -21,233 +38,249 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.regex.Pattern;
 
 public class RapidUtils {
 
-  private static final String TAG = "RapidUtils";
+	private static final String TAG = "RapidUtils";
+	private static final String IPADDRESS_PATTERN = 
+			"^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
 
-  private static final Thread demoServerThread;
-  private static BlockingQueue<String> commandQueue = new ArrayBlockingQueue<String>(1000);
-  private static String demoServerIp;
-  private static int demoServerPort;
+	private static final Thread demoServerThread;
+	private static BlockingQueue<String> commandQueue = new ArrayBlockingQueue<String>(1000);
+	private static String demoServerIp;
+	private static int demoServerPort;
 
-  static {
-    demoServerThread = new Thread() {
-      public void run() {
+	static {
+		demoServerThread = new Thread() {
+			public void run() {
 
-        System.out.println("Started thread that consumes the commands to send to the Demo server");
+				System.out.println("Started thread that consumes the commands to send to the Demo server");
 
-        while (true) {
-          try {
-            String cmd = commandQueue.take();
-            sendAnimationMsg(cmd);
-          } catch (InterruptedException e) {
-          }
-        }
-      }
-    };
+				while (true) {
+					try {
+						String cmd = commandQueue.take();
+						sendAnimationMsg(cmd);
+					} catch (InterruptedException e) {
+					}
+				}
+			}
+		};
 
-    demoServerThread.start();
-  }
+		demoServerThread.start();
+	}
 
-  /**
-   * Utility to execute a shell command on a Mac OS.
-   * 
-   * @param command
-   * @param asRoot
-   * @param password
-   * @return
-   */
-  public static String executeCommand(String command, boolean asRoot, String password) {
+	/**
+	 * Utility to execute a shell command on a Mac OS.
+	 * 
+	 * @param command
+	 * @param asRoot
+	 * @param password
+	 * @return
+	 */
+	public static String executeCommand(String command, boolean asRoot, String password) {
 
-    Process p = null;
-    String[] cmd = new String[] {"/bin/bash", "-c", command};
+		Process p = null;
+		String[] cmd = new String[] {"/bin/bash", "-c", command};
 
-    try {
-      if (asRoot) {
-        cmd = new String[] {"/bin/bash", "-c", "echo " + password + "|sudo -S " + command};
-      }
+		try {
+			if (asRoot) {
+				cmd = new String[] {"/bin/bash", "-c", "echo " + password + "|sudo -S " + command};
+			}
 
-      System.out.println("Executing command: " + command);
-      p = Runtime.getRuntime().exec(cmd);
-      // you can pass the system command or a script to exec command.
-      BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-      BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+			System.out.println("Executing command: " + command);
+			p = Runtime.getRuntime().exec(cmd);
+			// you can pass the system command or a script to exec command.
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
 
-      // read the output from the command
-      StringBuilder sb = new StringBuilder();
-      String s = "";
-      while ((s = stdInput.readLine()) != null) {
-        // System.out.println("Std OUT: "+s);
-        sb.append(s);
-      }
+			// read the output from the command
+			StringBuilder sb = new StringBuilder();
+			String s = "";
+			while ((s = stdInput.readLine()) != null) {
+				// System.out.println("Std OUT: "+s);
+				sb.append(s);
+			}
 
-      while ((s = stdError.readLine()) != null) {
-        System.out.println("Std ERROR : " + s);
-      }
+			while ((s = stdError.readLine()) != null) {
+				System.out.println("Std ERROR : " + s);
+			}
 
-      System.out.println(sb.toString());
+			System.out.println(sb.toString());
 
-      stdInput.close();
-      stdError.close();
-      writer.close();
+			stdInput.close();
+			stdError.close();
+			writer.close();
 
-      return sb.toString();
+			return sb.toString();
 
-    } catch (IOException e) {
+		} catch (IOException e) {
 
-      e.printStackTrace();
-    }
-    return null;
-  }
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-  public static String executeCommand(String command) {
-    return executeCommand(command, false, null);
-  }
+	public static String executeCommand(String command) {
+		return executeCommand(command, false, null);
+	}
 
-  public static byte[] serialize(Object obj) throws IOException {
-    byte[] serializedObj;
+	public static byte[] serialize(Object obj) throws IOException {
+		byte[] serializedObj;
 
-    ByteArrayOutputStream b = new ByteArrayOutputStream();
-    ObjectOutputStream o = new ObjectOutputStream(b);
-    try {
-      o.writeObject(obj);
-      o.flush();
-      serializedObj = b.toByteArray();
-    } finally {
-      o.close();
-    }
+		ByteArrayOutputStream b = new ByteArrayOutputStream();
+		ObjectOutputStream o = new ObjectOutputStream(b);
+		try {
+			o.writeObject(obj);
+			o.flush();
+			serializedObj = b.toByteArray();
+		} finally {
+			o.close();
+		}
 
-    return serializedObj;
-  }
+		return serializedObj;
+	}
 
-  public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
-    return RapidUtils.deserialize(bytes, 0, bytes.length);
-  }
+	public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+		return RapidUtils.deserialize(bytes, 0, bytes.length);
+	}
 
-  public static Object deserialize(byte[] bytes, int offset, int length)
-      throws IOException, ClassNotFoundException {
-    Object obj;
+	public static Object deserialize(byte[] bytes, int offset, int length)
+			throws IOException, ClassNotFoundException {
+		Object obj;
 
-    ByteArrayInputStream b = new ByteArrayInputStream(bytes, offset, length);
-    ObjectInputStream o = new ObjectInputStream(b);
-    obj = o.readObject();
-    o.close();
+		ByteArrayInputStream b = new ByteArrayInputStream(bytes, offset, length);
+		ObjectInputStream o = new ObjectInputStream(b);
+		obj = o.readObject();
+		o.close();
 
-    return obj;
-  }
+		return obj;
+	}
 
-  /**
-   * Connect to the animation server and send the messages that represent the sequence of
-   * operations.
-   * 
-   * @param config
-   * @param millis
-   * @param msgs
-   */
-  public static synchronized void sendAnimationMsg(Configuration config, String msg) {
-    sendAnimationMsg(config.getAnimationServerIp(), config.getAnimationServerPort(), msg);
-  }
+	/**
+	 * Connect to the animation server and send the messages that represent the sequence of
+	 * operations.
+	 * 
+	 * @param config
+	 * @param millis
+	 * @param msgs
+	 */
+	public static synchronized void sendAnimationMsg(Configuration config, String msg) {
+		sendAnimationMsg(config.getAnimationServerIp(), config.getAnimationServerPort(), msg);
+	}
 
-  /**
-   * Connect to the animation server and send the messages that represent the sequence of
-   * operations.
-   * 
-   * @param config
-   * @param millis
-   * @param msgs
-   */
-  public static synchronized void sendAnimationMsg(final String ip, final int port,
-      final String msg) {
+	/**
+	 * Connect to the animation server and send the messages that represent the sequence of
+	 * operations.
+	 * 
+	 * @param config
+	 * @param millis
+	 * @param msgs
+	 */
+	public static synchronized void sendAnimationMsg(final String ip, final int port,
+			final String msg) {
 
-    RapidUtils.demoServerIp = ip;
-    RapidUtils.demoServerPort = port;
+		RapidUtils.demoServerIp = ip;
+		RapidUtils.demoServerPort = port;
 
-    boolean added = false;
-    while (!added) {
-      try {
-        commandQueue.put(msg);
-        added = true;
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
-  }
+		boolean added = false;
+		while (!added) {
+			try {
+				commandQueue.put(msg);
+				added = true;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-  public static void closeQuietly(InputStream is) {
-    if (is != null) {
-      try {
-        is.close();
-      } catch (IOException e) {
-      }
-    }
-  }
+	public static void closeQuietly(InputStream is) {
+		if (is != null) {
+			try {
+				is.close();
+			} catch (IOException e) {
+			}
+		}
+	}
 
-  public static void closeQuietly(OutputStream os) {
-    if (os != null) {
-      try {
-        os.close();
-      } catch (IOException e) {
-      }
-    }
-  }
+	public static void closeQuietly(OutputStream os) {
+		if (os != null) {
+			try {
+				os.close();
+			} catch (IOException e) {
+			}
+		}
+	}
 
-  public static void closeQuietly(ObjectInputStream oIs) {
-    if (oIs != null) {
-      try {
-        oIs.close();
-      } catch (IOException e) {
-      }
-    }
-  }
+	public static void closeQuietly(ObjectInputStream oIs) {
+		if (oIs != null) {
+			try {
+				oIs.close();
+			} catch (IOException e) {
+			}
+		}
+	}
 
-  public static void closeQuietly(ObjectOutputStream oOs) {
-    if (oOs != null) {
-      try {
-        oOs.close();
-      } catch (IOException e) {
-      }
-    }
-  }
+	public static void closeQuietly(ObjectOutputStream oOs) {
+		if (oOs != null) {
+			try {
+				oOs.close();
+			} catch (IOException e) {
+			}
+		}
+	}
 
-  public static void closeQuietly(Socket socket) {
-    if (socket != null) {
-      try {
-        socket.close();
-      } catch (IOException e) {
-      }
-    }
-  }
+	public static void closeQuietly(Socket socket) {
+		if (socket != null) {
+			try {
+				socket.close();
+			} catch (IOException e) {
+			}
+		}
+	}
 
-  private static void sendAnimationMsg(String msg) {
-    Socket socket = null;
-    PrintWriter pout = null;
+	private static void sendAnimationMsg(String msg) {
+		Socket socket = null;
+		PrintWriter pout = null;
 
-    try {
-      System.out.println(TAG + " - Sending animation msg: " + msg);
-      socket = new Socket(demoServerIp, demoServerPort);
-      pout = new PrintWriter(socket.getOutputStream(), true);
-      pout.print(msg);
+		try {
+			System.out.println(TAG + " - Sending animation msg: " + msg);
+			socket = new Socket(demoServerIp, demoServerPort);
+			pout = new PrintWriter(socket.getOutputStream(), true);
+			pout.print(msg);
 
-    } catch (UnknownHostException e) {
-      System.err.println("Could not connect to animation server: " + demoServerIp + ":"
-          + demoServerPort + ": " + e);
-    } catch (IOException e) {
-      System.err.println("Could not connect to animation server: " + demoServerIp + ":"
-          + demoServerPort + ": " + e);
-    } catch (Exception e) {
-      System.err.println("Could not connect to animation server: " + demoServerIp + ":"
-          + demoServerPort + ": " + e);
-    } finally {
-      if (pout != null)
-        pout.close();
-      if (socket != null) {
-        try {
-          socket.close();
-        } catch (IOException e) {
-        }
-      }
-    }
-  }
+		} catch (UnknownHostException e) {
+			System.err.println("Could not connect to animation server: " + demoServerIp + ":"
+					+ demoServerPort + ": " + e);
+		} catch (IOException e) {
+			System.err.println("Could not connect to animation server: " + demoServerIp + ":"
+					+ demoServerPort + ": " + e);
+		} catch (Exception e) {
+			System.err.println("Could not connect to animation server: " + demoServerIp + ":"
+					+ demoServerPort + ": " + e);
+		} finally {
+			if (pout != null)
+				pout.close();
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+
+	/**
+	 * Validate ip address with regular expression
+	 * @param ip ip address for validation
+	 * @return true valid ip address, false invalid ip address
+	 */
+	public static boolean validateIpAddress(final String ip){
+		Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
+		return pattern.matcher(ip).matches();	    	    
+	}
 }
